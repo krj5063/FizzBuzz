b9ec48d1e0624fd356f6bb4ac2a37d6c366e3cf6
krj5063 <krj5063@gmail.com>
2014-12-07 16:50:43 -0500

    made changes to apps

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 $(document).ready(function() { 
 
-    
-var a = 1;
-var value = "";     
-    
-    for (a = 1; a <101; a++)
+var a = prompt("Please enter a number between 1 and 100") - 0;
+
+
+  
+var fBuzz = function (a) {    
     {
-        
         if (a==0)
         {
             $("body").append("0"+"<br>");
+        }
+        else if (a > 100)
+        {
+            alert("That number is higer than 100! You bozo!");
+        }
+        else if ((a % 1) != 0)
+        {
+            alert("You entered a decimal, please refresh and try again");
         }
         else if ((a % 15) ==0)
         {
@@ -23,10 +30,17 @@ var value = "";
         {
             $("body").append("Fizz"+"<br>");
         }
+        else if (isNaN(a))
+        {
+            alert("ERROR, you did not enter a number!");
+        }
         else {
             $("body").append(a+"<br>");
         }
     }
-    
+}
+
+console.log(fBuzz(a)); 
 
 });
+
